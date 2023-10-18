2659b88d844b4fba570d3744d7c2cd2cbf132843
Viktor Andreev <and.viktor@gmail.com>
2023-10-18 12:05:07 +0700

    Project and Scraper edit views rewrite in component style.

--- src/views/ProjectsEditView.vue
+++ src/views/ProjectsEditView.vue
@@ -1,10 +1,15 @@
 <script>
 import { toast } from 'vue3-toastify'
+import ItemEditForm from '../components/ui/ItemEditForm.vue';
 export default {
     data() {
       return {
-        project: {}
+        project: {},
+        project_loaded: false
       }
+    },
+    components: {
+      ItemEditForm
     },
     methods: {
         getProject() {
@@ -17,10 +22,11 @@ export default {
               .then(resp => resp.json())
               .then(resp => {
                   this.project = resp
+                  this.project_loaded = true
               })
               .catch(error => console.log(error))
         },
-        saveProject() {
+        saveItem(model) {
           fetch(`http://127.0.0.1:8000/api/projects/`+this.project.id+'/', {
               method: 'PATCH',
               headers: {
@@ -28,8 +34,8 @@ export default {
                   'Authorization': 'Token 15e5a110fd84cc4e736100b5a5ca1e5898cfcd62'
               },
               body: JSON.stringify({
-                  title: this.project.title,
-                  description: this.project.description
+                  title: model.title,
+                  description: model.description
               })
           })
           .then(resp => {
@@ -38,7 +44,7 @@ export default {
                   path: '/projects'
                 })
                 .then(() => {
-                  toast('Project "'+ this.project.title +'" has been successfully changed.', {
+                  toast('Project "'+ model.title +'" has been successfully changed.', {
                     type: 'success'
                   });
                 })
@@ -67,53 +73,32 @@ export default {
 </script>
 
 <template>
-        <div class="content-header row">
-          <div class="content-header-left col-md-8 col-12 mb-2 breadcrumb-new">
-            <h3 class="content-header-title mb-0 d-inline-block">{{ this.project.title }}</h3>
-            <div class="row breadcrumbs-top d-inline-block">
-              <div class="breadcrumb-wrapper col-12">
-                <ol class="breadcrumb">
-                  <li class="breadcrumb-item">
-                    <RouterLink :to="{name: 'projects'}">Projects</RouterLink>
-                  </li>
-                  <li class="breadcrumb-item active">Edit the project
-                  </li>
-                </ol>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="content-body">
-          <div class="row">
-              <div class="col-12">
-                  <section class="card">
-                      <div class="card-content">
-                          <div class="card-body">
-                              <div class="col-12">
-                                  <div class="row">
-                                      <div class="col-12">
-                                          <form class="form-horizontal row mt-2">
-                                              <div class="col-12">
-                                                  <fieldset class="form-label-group">
-                                                      <input v-model="project.title" type="text" class="form-control" id="project-title" placeholder="Project title" required="" autofocus="">
-                                                      <label for="project-title">Project title</label>
-                                                  </fieldset>
-                                                  <fieldset class="form-label-group">
-                                                      <textarea v-model="project.description" class="form-control" id="project-description" placeholder="Project description" autofocus="" rows="5"></textarea>
-                                                      <label for="project-description">Project description</label>
-                                                  </fieldset>
-                                              </div>
-                                              <div class="col-12 text-right">
-                                                  <button @click="saveProject()" type="button" class="btn-gradient-primary my-1">Save</button>
-                                              </div>
-                                          </form>
-                                      </div>
-                                  </div>
-                              </div>
-                          </div>
-                      </div>
-                  </section>
-              </div>
-          </div>
-        </div>
+  <ItemEditForm v-if="project_loaded"
+    :title = project.title
+    :model = project
+    :fields = "[
+      {
+        type: 'text',
+        name: 'project-title',
+        placeholder: 'Project title',
+        model: 'title'
+      },
+      {
+        type: 'textarea',
+        name: 'project-description',
+        placeholder: 'Project description',
+        model: 'description'
+      }
+    ]"
+    :breadcrumbs = "[
+        {
+            title: 'Projects',
+            to: 'projects'
+        },
+        {
+            title: 'Edit the project'
+        }
+    ]"
+    @save-event="saveItem"
+  />
 </template>
--- src/views/ScrapersEditView.vue
+++ src/views/ScrapersEditView.vue
@@ -1,11 +1,17 @@
 <script>
 import { toast } from 'vue3-toastify'
+import ItemEditForm from '../components/ui/ItemEditForm.vue';
 export default {
     data() {
       return {
         project: {},
-        scraper: {}
+        scraper: {},
+        project_loaded: false,
+        scraper_loaded: false
       }
+    },
+    components: {
+      ItemEditForm
     },
     methods: {
           getProject() {
@@ -19,6 +25,7 @@ export default {
               .then(resp => resp.json())
               .then(resp => {
                   this.project = resp
+                  this.project_loaded = true
               })
               .catch(error => console.log(error))
           },
@@ -32,11 +39,12 @@ export default {
               .then(resp => resp.json())
               .then(resp => {
                   this.scraper = resp
+                  this.scraper_loaded = true
                   this.getProject()
               })
               .catch(error => console.log(error))
         },
-        saveScraper() {
+        saveItem(model) {
           fetch(`http://127.0.0.1:8000/api/scrapers/`+this.scraper.id+'/', {
               method: 'PATCH',
               headers: {
@@ -44,18 +52,18 @@ export default {
                   'Authorization': 'Token 15e5a110fd84cc4e736100b5a5ca1e5898cfcd62'
               },
               body: JSON.stringify({
-                  title: this.scraper.title,
-                  description: this.scraper.description,
-                  headers: this.scraper.headers
+                  title: model.title,
+                  description: model.description,
+                  headers: model.headers
               })
           })
           .then(resp => {
               if(resp.status == 200) {
                 this.$router.push({
-                  path: '/scrapers/'+this.scraper.project
+                  path: '/scrapers/'+model.project
                 })
                 .then(() => {
-                  toast('Scraper "'+ this.scraper.title +'" has been successfully changed.', {
+                  toast('Scraper "'+ model.title +'" has been successfully changed.', {
                     type: 'success'
                   });
                 })
@@ -84,60 +92,50 @@ export default {
 </script>
 
 <template>
-        <div class="content-header row">
-          <div class="content-header-left col-md-8 col-12 mb-2 breadcrumb-new">
-            <h3 class="content-header-title mb-0 d-inline-block">{{ this.scraper.title }}</h3>
-            <div class="row breadcrumbs-top d-inline-block">
-              <div class="breadcrumb-wrapper col-12">
-                <ol class="breadcrumb">
-                  <li class="breadcrumb-item" v-if="project.id">
-                    <RouterLink :to="{name: 'projects_edit', params: {id: this.project.id }}">{{ this.project.title }}</RouterLink>
-                  </li>
-                  <li class="breadcrumb-item" v-if="project.id">
-                    <RouterLink :to="{name: 'scrapers', params: {project_id: this.project.id }}">Scrapers</RouterLink>
-                  </li>
-                  <li class="breadcrumb-item active">Edit the scraper
-                  </li>
-                </ol>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="content-body">
-          <div class="row">
-              <div class="col-12">
-                  <section class="card">
-                      <div class="card-content">
-                          <div class="card-body">
-                              <div class="col-12">
-                                  <div class="row">
-                                      <div class="col-12">
-                                          <form class="form-horizontal row mt-2">
-                                              <div class="col-12">
-                                                  <fieldset class="form-label-group">
-                                                      <input v-model="scraper.title" type="text" class="form-control" id="scraper-title" placeholder="Scraper title" required="">
-                                                      <label for="scraper-title">Scraper title</label>
-                                                  </fieldset>
-                                                  <fieldset class="form-label-group">
-                                                      <textarea v-model="scraper.description" class="form-control" id="scraper-description" placeholder="Project description" rows="5"></textarea>
-                                                      <label for="scraper-description">Scraper description</label>
-                                                  </fieldset>
-                                                  <fieldset class="form-label-group">
-                                                      <textarea v-model="scraper.headers" class="form-control" id="scraper-headers" placeholder="Headers" rows="5"></textarea>
-                                                      <label for="scraper-headers">Headers</label>
-                                                  </fieldset>
-                                              </div>
-                                              <div class="col-12 text-right">
-                                                  <button @click="saveScraper()" type="button" class="btn-gradient-primary my-1">Save</button>
-                                              </div>
-                                          </form>
-                                      </div>
-                                  </div>
-                              </div>
-                          </div>
-                      </div>
-                  </section>
-              </div>
-          </div>
-        </div>
+  <ItemEditForm v-if="project_loaded && scraper_loaded"
+    :title = scraper.title
+    :model = scraper
+    :fields = "[
+      {
+        type: 'text',
+        name: 'scraper-title',
+        placeholder: 'Scraper title',
+        model: 'title'
+      },
+      {
+        type: 'textarea',
+        name: 'scraper-description',
+        placeholder: 'Scraper description',
+        model: 'description'
+      },
+      {
+        type: 'textarea',
+        name: 'scraper-headers',
+        placeholder: 'Headers',
+        model: 'headers'
+      }
+    ]"
+    :breadcrumbs = "[
+        {
+            title: this.project.title,
+            to: 'projects_edit',
+            params: {
+                key: 'id',
+                value: this.project.id
+            }
+        },
+        {
+            title: 'Scrapers',
+            to: 'scrapers',
+            params: {
+                key: 'project_id',
+                value: this.project.id
+            }
+        },
+        {
+            title: 'Edit the scraper'
+        }
+    ]"
+    @save-event="saveItem"
+  />
 </template>
